eaebe222e3fa2a47bf20f30cce477416dae8efb6
karadasmurat <karadasmurat@gmail.com>
2022-07-31 17:04:51 +0300

    enum

---
+++ lib/dart/enum.dart
@@ -0,0 +1,30 @@
+// Prefer using lowerCamelCase for constant names
+enum Status { pending, inProgress, done }
+
+enum Direction { north, south, east, west }
+
+void main(List<String> args) {
+  Status currentStatus = Status.inProgress;
+
+  if (currentStatus == Status.inProgress) {
+    print("In progress ...");
+  }
+
+  // To get a list of all the enumerated values, use the enum’s values constant.
+  List<Status> stats = Status.values;
+  print(stats[0]);
+
+  decideBasedOnEnum(Direction.west);
+}
+
+void decideBasedOnEnum(Direction dir) {
+  // You can use enums in switch statements.
+  // You’ll get a warning if you don’t handle all of the enum’s values:
+  switch (dir) {
+    case Direction.north:
+      print("Going North");
+      break;
+    default: // Without this, you see a WARNING.
+      print("Not North? Alright..");
+  }
+}
