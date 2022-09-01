51b849da09244c8f57fca437ecf624098f35b0b7
karadasmurat <karadasmurat@gmail.com>
2022-09-01 23:27:12 +0300

    animations

--- lib/flutter/animation_controller.dart
+++ lib/flutter/animation_controller.dart
@@ -8,27 +8,62 @@ class AnimationControllerDemo extends StatefulWidget {
 }
 
 class _AnimationControllerDemoState extends State<AnimationControllerDemo>
-    with TickerProviderStateMixin {
+    //with SingleTickerProviderStateMixin {
+    with
+        TickerProviderStateMixin {
   bool isActive = false;
   bool isOn = false;
 
+  bool loaded = false;
+
+  // AnimationController derives from Animation<double>, so it can be used wherever an Animation object is needed.
+  // However, the AnimationController has additional methods to control the animation.
+  // By default, an AnimationController linearly produces the numbers from 0.0 to 1.0 during a given duration.
+  // The presence of vsync prevents offscreen animations from consuming unnecessary resources. You can use your stateful object as the vsync by adding SingleTickerProviderStateMixin to the class definition.
   late AnimationController _animationController;
+  late AnimationController _animationControllerD;
+
+  // An Animation object sequentially generates interpolated numbers between two values over a certain duration.
+  // It has state and its current value is always available in the .value member.
+  // An Animation object knows nothing about rendering or build() functions.
   late Animation _animation;
+  late Animation _animationD;
+
+  // By default, the AnimationController object ranges from 0.0 to 1.0.
+  // If you need a different range or a different data type, you can use a Tween to configure an animation to interpolate to a different range or data type.
+  // tween = Tween<double>(begin: -200, end: 0);
+  // animation's value comes from the tween
+  final colorTween = ColorTween(begin: Colors.grey, end: Colors.red);
 
   @override
   void initState() {
     super.initState();
 
-    _animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1))
-          ..addListener(() {
-            setState(() {});
-          });
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
 
-    _animation = ColorTween(
-      begin: Colors.grey,
-      end: Colors.red,
-    ).animate(_animationController);
+    _animationControllerD = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    );
+
+    // To use a Tween object, call animate() on the Tween, passing in the controller object.
+    _animation = colorTween.animate(_animationController)
+      ..addListener(() {
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+      });
+
+    //animation's value comes from the tween:
+    _animationD = Tween<double>(begin: 10, end: 0).animate(_animationControllerD)
+      ..addListener(() {
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+      });
   }
 
   @override
@@ -40,9 +75,12 @@ class _AnimationControllerDemoState extends State<AnimationControllerDemo>
           // the old and new values of properties when they change using the provided curve and duration.
           // Properties that are null are not animated. Its child and descendants are not animated.
           duration: const Duration(seconds: 1),
+          alignment: Alignment.center,
           width: double.infinity,
           height: 200,
           color: isActive ? Colors.greenAccent : Colors.blue[200],
+          child: Text(
+              "animation.value:\n${_animation.value}\n\ncontroller.value:\n${_animationController.value}"),
         ),
         Row(
           children: [
@@ -66,6 +104,45 @@ class _AnimationControllerDemoState extends State<AnimationControllerDemo>
             const Text("ColorTween on icon and AnimatedContainer"),
           ],
         ),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 8),
+          padding: EdgeInsets.all(16.0),
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.blue[200],
+          ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+                "animation.value:\n${_animationD.value}\n\ncontroller.value:\n${_animationControllerD.value}"),
+            LinearProgressIndicator(
+              color: Colors.green,
+              backgroundColor: Colors.green[100],
+              value: _animationControllerD.value,
+            ),
+            ButtonBar(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (loaded) {
+                      _animationControllerD.forward();
+                    } else {
+                      _animationControllerD.reverse();
+                    }
+                    //toggle variable
+                    loaded = !loaded;
+                  },
+                  child: const Text("Tween.animate"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _animationControllerD.stop();
+                  },
+                  child: const Text("controller.stop"),
+                ),
+              ],
+            ),
+          ]),
+        )
       ],
     );
   }
--- lib/flutter/animations.dart
+++ lib/flutter/animations.dart
@@ -10,7 +10,12 @@ class AnimationDemo extends StatefulWidget {
 }
 
 class _AnimationDemoState extends State<AnimationDemo> {
-  double opacityValue = 1;
+  double opacityValue = 1.0;
+  double turns = 1.0;
+  double width = 50.0;
+  Color color = Colors.blue;
+
+  final Tween<double> _scaleTween = Tween(begin: 1.0, end: 2.0);
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +29,7 @@ class _AnimationDemoState extends State<AnimationDemo> {
             tabs: [
               Tab(text: "Animated"),
               Tab(text: "Controller"),
-              Tab(text: "ClipPath"),
+              Tab(text: "TweenAnimationBuilder"),
             ],
           ),
         ),
@@ -43,9 +48,45 @@ class _AnimationDemoState extends State<AnimationDemo> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
+                    // With implicit animations, you can animate a widget property by setting a target value;
+                    // whenever that target value changes, the widget animates the property from the old value to the new one.
+                    // In this way, implicit animations trade control for convenience—they manage animation effects so that you don’t have to.
+                    AnimatedContainer(
+                      duration: const Duration(milliseconds: 600),
+                      curve: Curves.fastOutSlowIn,
+                      width: width,
+                      height: 50,
+                      color: color,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            width = (width == 50) ? 100 : 50;
+                            color = (color == Colors.blue) ? Colors.green : Colors.blue;
+                          });
+                        },
+                        child: const Text("AnimatedContainer")),
+                  ],
+                ),
+              ),
+              Container(
+                height: 100,
+                width: double.infinity,
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.blue[200],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    // With implicit animations, you can animate a widget property by setting a target value;
+                    // whenever that target value changes, the widget animates the property from the old value to the new one.
+                    // In this way, implicit animations trade control for convenience—they manage animation effects so that you don’t have to.
                     AnimatedOpacity(
                       duration: const Duration(seconds: 1),
                       opacity: opacityValue,
+                      //curve: Curves.bounceIn,
                       child: Container(
                         width: 50,
                         height: 50,
@@ -62,13 +103,77 @@ class _AnimationDemoState extends State<AnimationDemo> {
                   ],
                 ),
               ),
+              Container(
+                height: 100,
+                width: double.infinity,
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.blue[200],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    // With implicit animations, you can animate a widget property by setting a target value;
+                    // whenever that target value changes, the widget animates the property from the old value to the new one.
+                    // In this way, implicit animations trade control for convenience—they manage animation effects so that you don’t have to.
+                    // Animated version of Transform.rotate
+                    AnimatedRotation(
+                      duration: const Duration(milliseconds: 600),
+                      turns: turns,
+                      curve: Curves.fastOutSlowIn,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            turns += 1.0 / 8.0;
+                          });
+                        },
+                        child: const Text("AnimatedRotation")),
+                  ],
+                ),
+              )
             ],
           ),
           //Tab 2
           const AnimationControllerDemo(),
 
           //Tab 3
-          ClipPathDemo(),
+          Container(
+            height: 100,
+            width: double.infinity,
+            margin: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.blue[200],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // Set a tween of Type, and use the same for the builder, ie, double
+                TweenAnimationBuilder<double>(
+                  tween: _scaleTween,
+                  duration: const Duration(milliseconds: 600),
+                  builder: (context, val, child) {
+                    return Transform.scale(
+                      scale: val,
+                      child: child,
+                    );
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          )
         ]),
       ),
     );
